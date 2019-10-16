31194a073c8b0c48af89905f28f3a54d271a0be0
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-10-16 19:37:47 -0400

    exercise-202

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,40 @@
+package com.example.demo;
+
+public class Student {
+    private String name;
+    private int age;
+    private double height;
+    private int phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+}
